42fba0e825395d0640ef8981e513b42a738c6f2a
nrv <nrv303511@gmail.com>
2025-07-03 16:35:32 -0700

    bar

--- main.cpp
+++ main.cpp
@@ -24,6 +24,9 @@ typedef struct Track {
 void loadSongs(FilePathList droppedFiles, std::list<Track>* tracklist_pt);
 std::string parseNameFromPath(std::string src);
 
+void drawControlPanel(float duration, float timePlayed, Rectangle controlpanel, Rectangle progbar);
+
+
 int main(void) {
     SetTraceLogLevel(LOG_DEBUG);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Audio Player");
@@ -37,7 +40,8 @@ int main(void) {
     // Flags
     bool songStarted, isPause, autoplay;
      songStarted = isPause = false;
-    float scroll_offset = 0;
+    float duration, scroll_offset, timePlayed;
+    duration = scroll_offset = timePlayed = 0;
 
     // TODO: Embedd in the exe
     // Assets
@@ -52,17 +56,30 @@ int main(void) {
     // Texture left_skip = LoadTexture("../../assets/black-icon/SolidArrow_Left.png");
     // Texture right_skip = LoadTexture("../../assets/black-icon/SolidArrow_Right.png");
 
-
+    // UI
     const float borderpad = SCREEN_WIDTH/10.0f;
-    const float control_panel = SCREEN_HEIGHT/5.0f;
+
+    Rectangle controlpanel = {
+        .x = 0,
+        .y = SCREEN_HEIGHT - SCREEN_HEIGHT/5.0f,
+        .width = SCREEN_WIDTH,
+        .height = SCREEN_HEIGHT/5.0f,
+    };
             
     Rectangle content = {
         .x = borderpad,
         .y = borderpad,
         .width = SCREEN_WIDTH-borderpad*2.0f,
-        .height = SCREEN_HEIGHT-control_panel-borderpad*2.0f,
+        .height = SCREEN_HEIGHT-controlpanel.height-borderpad*2.0f,
     };
     RenderTexture2D songqueue = LoadRenderTexture(content.width, content.height);
+
+    Rectangle progbar = {
+        .x = borderpad,
+        .y = controlpanel.y + controlpanel.height/2.0f - 50.0f/2.0f,
+        .width = SCREEN_WIDTH,
+        .height = 50.0f
+    };
 
     SetTargetFPS(60);
     while(!WindowShouldClose()) {
@@ -102,6 +119,11 @@ int main(void) {
                     float msmove = GetMouseWheelMove();
                     if (msmove != 0) scroll_offset += msmove * 20.0f;
                 }
+                duration = GetMusicTimePlayed((*it).music);
+                timePlayed = duration/GetMusicTimeLength(((*it).music));
+                
+                // if (timePlayed >= .99) {
+                //     skipForward(&track, &songStarted);
                 UpdateMusicStream((*it).music);
             }
         }
@@ -109,23 +131,12 @@ int main(void) {
         BeginDrawing();
         ClearBackground(DARKGRAY);
 
-        // DrawRectangleRec(menubar, BLACK); // Menu Bar
-        //GuiGroupBox((Rectangle) {SCREEN_WIDTH+SCREEN_WIDTH*.25f, 100, SCREEN_WIDTH*.075f, SCREEN_HEIGHT-100}, "Queue");
-
         if (state == START) {
             const char* text = "DROP MUSIC";
             const int fontsize = 50;
             DrawText(text, SCREEN_WIDTH/2.0f - MeasureText(text, fontsize)/2, SCREEN_HEIGHT/3.0f - fontsize, fontsize, SKYBLUE);
             DrawTexture(drop_text, SCREEN_WIDTH/2.0f-drop.width/2.0f, SCREEN_HEIGHT/2.0f, WHITE);
         } else {
-            // TODO: Create a full-screen vs.playlist view state 
-            // Full-Screen
-            // --------------------------------------------------
-            // Playlist
-            // DO the button is psd and export as png
-            // Border?
-
-            
             const float songpad = (content.height/5.0f)/10.0f;
             const float songw = content.width - songpad*2.0f;
             const float songh = content.height/5.0f - songpad;
@@ -157,7 +168,7 @@ int main(void) {
                 DrawTexturePro(songqueue.texture, source, dest,
                                origin, 0.0f, WHITE);
             }
-            DrawRectangle(0, SCREEN_HEIGHT - control_panel, SCREEN_WIDTH, control_panel, BLACK);
+            drawControlPanel(duration, timePlayed, controlpanel, progbar);
         }
         EndDrawing();
     }
@@ -170,6 +181,17 @@ int main(void) {
     return 0;
 }
 
+void drawControlPanel(const float duration, const float timePlayed, Rectangle controlpanel, Rectangle progbar) {
+        DrawRectangle(0, SCREEN_HEIGHT - controlpanel.height, SCREEN_WIDTH, controlpanel.height, RAYWHITE);
+        
+
+        float pretty_duration = (int)duration % 60; 
+        DrawText(TextFormat("%d:%2.0f", (int)duration, pretty_duration), 5, progbar.y+20, 10, BLACK);
+        float fill = progbar.width*timePlayed;
+        DrawRectangleRec((Rectangle) {progbar.x, progbar.y, fill, progbar.height}, BLACK);
+        DrawRectangleLines(progbar.x, progbar.y, progbar.width, progbar.height, PINK);
+
+}
 void loadSongs(FilePathList droppedFiles, std::list<Track>* tracklist_pt) {
     for (int i = 0; i < (int)droppedFiles.count; i++)
     {
