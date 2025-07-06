004b853fe10a854fa18d4acfc4900beb098e771a
nrv <nrv303511@gmail.com>
2025-07-06 16:11:54 -0700

    loaded in the buttons

--- main.cpp
+++ main.cpp
@@ -22,8 +22,18 @@ typedef struct Track {
 void loadSongs(FilePathList droppedFiles, std::list<Track>* tracklist_pt);
 std::string parseNameFromPath(std::string src);
 
-void seek_by_amount(std::list<Track>::iterator* it_pt, const float length, const float abs_timeplayed, const float amount);
+void seek_by_amount(std::list<Track>::iterator it, const float length, const float abs_timeplayed, const float amount);
+void del(std::list<Track>::iterator temp, std::list<Track>* tracklist);
 
+
+// UI
+typedef enum BUTTON_STATE {
+    NORMAL,
+    SELECTED,
+    HOVERED,
+    HOVERED_EXIT,
+    HOEVERED_EXIT_CLICK,
+};
 
 void drawTitle(std::string title, Rectangle progbar, Rectangle controlpanel);
 void drawProgbar(const float abs_timeplayed, const float timeplayed, Rectangle progbar);
@@ -39,22 +49,17 @@ int main(void) {
     std::list<Track> tracklist;
     std::list<Track>::iterator it;
 
-    // Flags & vars
+    // Flags
     bool songStarted, isPause, autoplay;
-     songStarted = isPause = false;
+    songStarted = isPause = false;
+
     float length, abs_timeplayed, scroll_offset, timeplayed;
     length = abs_timeplayed = scroll_offset = timeplayed = 0;
     float vol = 0.5;
 
-    // TODO: Embedd in the exe
-    // Assets
-    //Texture play = LoadTexture("../../assets/black-icon/Music-On.png");
-    // Texture2D drop = LoadTexture("../../assets/black-icon/Home.png");
-    
-    // Texture play = LoadTexture("../../assets/black-icon/Play.png");
-    // Texture pause = LoadTexture("../../assets/black-icon/Pause.png");
-    // Texture left_skip = LoadTexture("../../assets/black-icon/SolidArrow_Left.png");
-    // Texture right_skip = LoadTexture("../../assets/black-icon/SolidArrow_Right.png");
+    Vector2 mspos;
+
+    Texture2D atlas = LoadTexture("assets.png");
 
     // UI
     const float borderpad = SCREEN_WIDTH/10.0f;
@@ -90,6 +95,43 @@ int main(void) {
         .height = SCREEN_HEIGHT - (controlpanel.y + vertpad) - (SCREEN_HEIGHT-(progbar.y + progbar.height)),
     };
 
+    Vector2 button_size = {454, 111};
+    Vector2 exit_box_size = {38, 41}; 
+    Rectangle hovered_exit = {
+        .x = 0,
+        .y = 0,
+        .width = button_size.x,
+        .height = button_size.y,
+    };
+
+    Rectangle hovered_exit_click = {
+        .x = button_size.x,
+        .y = 0,
+        .width = button_size.x,
+        .height = button_size.y,
+    };
+
+    Rectangle selected = {
+        .x = 0,
+        .y = button_size.y,
+        .width = button_size.x,
+        .height = button_size.y,
+    };
+
+    Rectangle hovered = {
+        .x = button_size.x,
+        .y = button_size.y,
+        .width = button_size.x,
+        .height = button_size.y,
+    };
+
+    Rectangle normal = {
+        .x = 0,
+        .y = button_size.y*2.0f,
+        .width = button_size.x,
+        .height = button_size.y,
+    };
+
     SetTargetFPS(60);
     SetMasterVolume(.5);
     while(!WindowShouldClose()) {
@@ -110,7 +152,7 @@ int main(void) {
                 PlayMusicStream((*it).music);
                 songStarted = true;
             } else {
-                Vector2 mousepos = GetMousePosition();
+                mspos = GetMousePosition();
                 if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_RIGHT)
                     && it != std::prev(tracklist.end())) {
                     StopMusicStream((*it).music);
@@ -130,20 +172,20 @@ int main(void) {
                     }
                     isPause = !isPause;
                 } else if (IsKeyPressed(KEY_RIGHT)) {
-                    seek_by_amount(&it, length, abs_timeplayed, 10.0f);
+                    seek_by_amount(it, length, abs_timeplayed, 10.0f);
                 } else if (IsKeyPressed(KEY_LEFT)) {
-                    seek_by_amount(&it, length, abs_timeplayed, -10.0f);
-                } else if (CheckCollisionPointRec(mousepos, progbar)) {
+                    seek_by_amount(it, length, abs_timeplayed, -10.0f);
+                } else if (CheckCollisionPointRec(mspos, progbar)) {
                     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                        float pos = (mousepos.x - progbar.x) / progbar.width;
+                        float pos = (mspos.x - progbar.x) / progbar.width;
                         SeekMusicStream((*it).music, length*pos);
                     }
-                } else if (CheckCollisionPointRec(mousepos, content)) {                    
+                } else if (CheckCollisionPointRec(mspos, content)) {                    
                     float msmove = GetMouseWheelMove();
                     if (msmove != 0) scroll_offset -= msmove * 20.0f;
-                } else if (CheckCollisionPointRec(mousepos, volbar)) {
+                } else if (CheckCollisionPointRec(mspos, volbar)) {
                     if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                        float pos = (mousepos.y - volbar.y) / volbar.height;
+                        float pos = (mspos.y - volbar.y) / volbar.height;
                         vol = pos;
                         SetMasterVolume(pos);
                     }
@@ -185,19 +227,48 @@ int main(void) {
                 auto temp_it = std::next(it, 1);
                 float y = content.y;
                 for (int i = 0; temp_it != tracklist.end(); i++, temp_it++) {
-                    Rectangle songbox = {
+                    Rectangle src;
+                    BUTTON_STATE bs;
+                    Rectangle song_box = {
                         content.x + songpad,
                         y + i * (songh + songpad) - scroll_offset,
                         songw, 
                         songh
                     };
-                    if (songbox.y + songbox.height < content.y || songbox.y > content.y + content.height)
+
+                    // exit_box_size is the width and height of the exit_box
+                    Rectangle song_box_exit_box = {
+                        .x = song_box.x + exit_box_size.x,
+                        .y = song_box.y + exit_box_size.y,
+                        .width = exit_box_size.x,
+                        .height = exit_box_size.y,
+                    };
+
+                    if (song_box.y + song_box.height < content.y || song_box.y > content.y + content.height)
                         continue;
+                    if (CheckCollisionPointRec(mspos, song_box)) {
+                        if (CheckCollisionPointRec(mspos, song_box_exit_box)) {
+                            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                                std::list<Track>::iterator del_it = temp_it;
+                                temp_it = std::prev(temp_it, 1);
+                                del(del_it, &tracklist);
+                                i--;
+                                continue;
+                            } else {
+                                src = hovered_exit;
+                            }
+                        } else {
+                            src = hovered;
+                        }
+                    } else {
+                        src = normal;
+                    }
 
                     const char* songname = (*temp_it).title.c_str();
                     const int fontsize = 24;
-                    DrawRectangleRounded(songbox, .3f, 10, DARKBLUE);
-                    DrawText(songname, songbox.x + 5, songbox.y + songbox.height/2.0f-5, fontsize, LIGHTGRAY);
+                    //DrawRectangleRounded(song_box, .3f, 10, DARKBLUE);
+                    DrawTextureRec(atlas, src, (Vector2) {song_box.x, song_box.height,}, WHITE);
+                    DrawText(songname, song_box.x + 5, song_box.y + song_box.height/2.0f-5, fontsize, LIGHTGRAY);
                 }
             }
             else 
@@ -220,14 +291,19 @@ int main(void) {
     for (auto& s : tracklist) {
         UnloadMusicStream(s.music);
     }
+    UnloadTexture(atlas);
     CloseAudioDevice();
     CloseWindow();
     return 0;
 }
 
-void seek_by_amount(std::list<Track>::iterator* it_pt, const float length, const float abs_timeplayed, const float amount) {
+void del(std::list<Track>::iterator temp, std::list<Track>* tracklist) {
+    (*tracklist).erase(temp);
+}
+
+void seek_by_amount(std::list<Track>::iterator it, const float length, const float abs_timeplayed, const float amount) {
     Clamp(abs_timeplayed + amount, 0, length);
-    SeekMusicStream((*(*it_pt)).music, abs_timeplayed + amount); 
+    SeekMusicStream((*it).music, abs_timeplayed + amount); 
 }
 
 void drawTitle(std::string title, Rectangle progbar, Rectangle controlpanel) {
